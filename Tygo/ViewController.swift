2111f68ee21f39abf15ce540d98e034c4879c860
CAOCAP <ficruty@gmail.com>
2022-07-12 17:18:54 +0300

    Initial Commit

---
+++ Tygo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tygo
+//
+//  Created by Azzam AL-Rashed on 13/12/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
